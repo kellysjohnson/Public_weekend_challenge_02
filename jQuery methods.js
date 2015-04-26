4e8a127ce25e587c1140ccebbb245b117901239f
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-04-26 07:21:37 -0500

    Can store number selected. Added array.

---
+++ jQuery methods.js
@@ -0,0 +1,54 @@
+GET/CHANGE CONTENT
+.html() - pg 316
+.text() - pg 316
+.replaceWith() - pg 316
+.remove() - pg 316
+
+ELEMENTS
+.before() - pg 318
+.after() - pg 318
+.prepend() - pg 318
+.append() - pg 318
+.remove() - pg 346
+.clone() - pg 346
+.unwrap() - pg 346
+.detach() - pg 346
+.empty() - pg 346
+.add() - pg 338
+
+ATTRIBUTES
+.attr() - pg 320
+.removeAttr() - pg 320
+.addClass() - pg 320
+.removeClass() - pg 320
+.css() - pg 322
+
+FORM VALUES
+.val() - pg 343
+.isNumeric() - pg 343
+
+GENERAL
+.find() - pg 336
+.closest() - pg 336
+.parent() - pg 336
+.parents() - pg 336
+.children() - pg 336
+.siblings() - pg 336
+.next() - pg 336
+.nextAll() - pg 336
+.prev() - pg 336
+.prevAll() - pg 336
+
+FILTER / TEST
+.filter() - pg 338
+.not() - pg 338
+.has() - pg 338
+.is() - pg 338
+:contains() - pg 338
+
+ORDER OF SELECTION
+.eq() - pg 340
+.lt() - pg 340
+.gt() - pg 340
+
+(More on page 305 :) )﻿
--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-
+var classArray = ["Erik Johnson", "Aaron Sawyer", "Alicia Smith", "Casie Lynch", "Clare Jacky", "Cody Misura", "Jeanne Erickson Cooley", "Kaitlin Muth", "Kelly Johnson", "Luke Poppe", "Mary White", "Michael Liquori", "Michelle Funk", "Rom Stevens", "Steve Martin", "Terry Gunderson", "Tracy Fuller", "Vince Jones", "Brian Aamodt", "Chelsea Okey"];﻿
+var familyArray = ["Annabelle", "Samantha", "Isaac", "Amanda", "Kodi", "Kelly", "Jacob"];
 
 
 
 $(document).ready(function(){
 
 	$("div").on("click", ".button", function(){
-		console.log("hi!");
-		var numSelected = $(this).attr("value"));
+		var numSelected = $(this).attr("value");
 		console.log(numSelected);
 
 	});
 
+	$("div").on("click", ".generate", function(){
+
+		
+		//use an Array to store 'numSelected' number of names
+		//load headings for each team.  Team 1
+
+		//display team list
+		//then display in a list each of the numSelected' number of names per team
+
+
+	});
 
 });
